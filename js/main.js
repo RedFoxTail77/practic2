7bc4c2a59aa49a2c933f4bbec65a7e42188be595
RedFoxTail77 <52562316+RedFoxTail77@users.noreply.github.com>
2019-07-26 08:06:03 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.slider_inner').slick({
+    nextArrow:'<button type="button" class="slick-btn slic-next">-</button>',
+    prevArrow:'<button type="button" class="slick-btn slic-prev">-</button>',
+        
+});
+$('select').styler();
+
+
+});
